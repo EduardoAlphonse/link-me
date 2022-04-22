a2450338bf90a21267bafbfc8c12d1123b38cae6
EduardoAlphonse <eduardoliveira.dev@gmail.com>
2022-04-22 18:51:09 -0300

    header in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img
+          src="https://i1.sndcdn.com/artworks-Zg2yezSG5RRY-0-t500x500.jpg"
+          alt="Foto de usuário"
+        />
+        <h1 style={{ color: '#f00 ' }}>@_username</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
